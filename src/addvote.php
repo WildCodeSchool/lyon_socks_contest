2662a6de5b09a9248f5505b4e905f83f9206c91f
AymenMech <aymen.mechiche@gmail.com>
2016-10-07 10:06:45 +0200

    getting vote id, now adding sql update

--- src/addvote.php
+++ src/addvote.php
@@ -1,1 +1,22 @@
 <?php
+var_dump($_POST);
+
+require_once 'bdd.php';
+$id = $_GET['id'];
+$voteNumber = count($_POST);
+
+if ( $voteNumber <= 2){
+    header("Location:vote.php?id=" .$id. "&error=1");
+}
+elseif ($voteNumber >= 4) {
+    header("Location:vote.php?id=" .$id. "&error=2");
+}
+    $in = "(";
+    foreach ($_POST as $value => $voteFor) {
+        $in .= $voteFor . ",";
+    }
+    $likes =(substr($in, 0, -1));
+    $likes .= ")";
+var_dump($likes);
+    $sql = "updte  in " . $in;
+
--- src/vote.php
+++ src/vote.php
@@ -1,3 +1,6 @@
+<?php
+$id = $_GET['id'];
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,18 +19,29 @@
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <script type="text/javascript">
-        function chkcontrol(j) {
+        function chkcontrol(i) {
             var total=0;
             for(var i=0; i < document.form1.ckb.length; i++){
-                if(document.form1.ckb[i].checked){
-                    total =total +1;}
+                if(document.form1.('ckb')[i].checked){
+                    total = total +1;}
                 if(total > 3){
                     alert("Please Select only three")
                     document.form1.ckb[i].checked = false ;
                     return false;
                 }
             }
-        } </script>
+        }
+    </script>
+
+    <script>
+        var limit = 3;
+        $('input.ckb').on('change', function(evt) {
+            if($(this).siblings(':checked').length >= limit) {
+                alert('Vous pouvez voter que pour 3 paires de chaussettes !');
+                this.checked = false;
+        }
+    });
+    </script>
     <link rel="stylesheet" href="../public/css/style.css">
 
 </head>
@@ -36,15 +50,27 @@
     <div class="row">
         <div class="col-xs-offset-3 col-xs-6">
             <h2>Participez au concours des plus belles chaussettes !</h2>
+            <h3>Votez pour vos 3 paires de chaussettes préférées.</h3>
+            <h4>
+                <?php
+                if (isset($_GET['error'])){
+                    if ($_GET['error'] == 1) {
+                        echo "Vous avez votez pour 2 paires ou moins, recommencez  = )";
+                    }   elseif ($_GET['error'] == 2) {
+                        echo "Vous avez votez pour plus de 3 paires, recommencez = )";
+                    }
+                }
+                ?>
+
+            </h4>
         </div>
     </div>
 
 
             <div class="row imgsockss">
-            <form method="post" name="form1" action="addvote.php">
+            <form method="post" name="form1" action="addvote.php?id=<?php echo $id; ?>">
 
 <?php
-$id = $_GET['id'];
 require_once "bdd.php";
 
                 $sql= "select * from guests where id != $id ";
@@ -52,7 +78,7 @@ require_once "bdd.php";
                 while ($row = $query->fetch_assoc()) {
 
                    // echo "<div class='col-lg-3 col-md-4 col-xs-6 imgsocks'><a class='thumbnail'><img class='img-responsive' src=\"".( $row['picture_url'])."\"/></a></div>";
-                    echo "<div class='col-lg-3 col-md-4 col-xs-6 imgsocks' style='background-image:url(\"".( $row['picture_url'])."\")'><input type=checkbox name=ckb value=" .$row['id']. " onclick='chkcontrol(" .$row['id']. ")'></div>";
+                    echo "<div class='col-lg-3 col-md-4 col-xs-6 imgsocks' style='background-image:url(\"".( $row['picture_url'])."\")'><input type='checkbox' class='ckb' name=" .$row['id']. " value=" .$row['id']. " onclick='chkcontrol(" .$row['id']. ")'></div>";
                 }
 ?>              <input type="submit" value="Valider mes votes">
             </form>
