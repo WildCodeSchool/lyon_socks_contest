51de5312d173d81eaf596c6c554eb3e2a43d8b69
AymenMech <aymen.mechiche@gmail.com>
2016-10-17 14:53:07 +0200

    connection working

--- src/control-id.php
+++ src/control-id.php
@@ -10,20 +10,26 @@ if ( (empty($last_name) === true) || (empty($name) === true)){
     header('Location:../public/login.php?error=2');
 }
 
-$sql = "SELECT * FROM guests";
+$sql = "SELECT COUNT(*) as user_exist FROM guests where last_name = '$last_name' AND first_name = '$name'";
 $query = executeSql(getConnection(),$sql);
-while ($row = $query->fetch_array()) {
-    if (($row['last_name'] == $last_name) && ($row['first_name'] == $name)) {
-        if ($row['vote'] == 0) {
-            $_SESSION['id'] = $row['id'];
-            header('Location:vote.php');
-        } elseif ($row['vote'] == 1) {
-            session_destroy();
-            header('Location:../public/login.php?error=3');
+while ($row = $query->fetch_assoc()) {
+    if ($row['user_exist'] == 1) {
+        $sql = "SELECT * FROM guests where last_name = '$last_name' AND first_name = '$name'";
+        $query = executeSql(getConnection(),$sql);
+        while ($row = $query->fetch_assoc()) {
+            if ($row['vote'] == 0) {
+                {
+                    $_SESSION['id'] = $row['id'];
+                    header('Location:vote.php');
+                }
+            } elseif ($row['vote'] == 1) {
+                session_destroy();
+                header('Location:../public/login.php?error=3');
+            }
         }
-        else {
-            session_destroy();
-            header('Location:../public/login.php?error=1');
-        }
+    }
+    else {
+        session_destroy();
+        header('Location:../public/login.php?error=1');
     }
 }
